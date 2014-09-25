7695cf2af9acf0d98f86c83cd53670d570c6244a
Philip Nachbauer <hello@naba.at>
2014-09-25 11:35:22 +0200

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+    'use strict';
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        copy: {
+            hooks: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bin/hooks/*'
+                        ],
+                        dest: '.git/hooks/'
+                    }
+                ]
+            }
+        },
+
+        uglify: {
+            dev: {
+                options: {
+                    mangle: false,
+                    compress: true,
+                    preserveComments: 'all',
+                    beautify: false
+                },
+                files: {
+                    'app/dist/bookmark-automatic-pull-request.js': [
+                        'app/scripts/automatic-pull-request.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', function() {
+        grunt.task.run([
+            'uglify',
+        ]);
+    });
+};
